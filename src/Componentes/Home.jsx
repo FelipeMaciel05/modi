f16c6f80314fba83165a8e3c0f3a70ae2081b64a
juanma-blip <101829196+juanma-blip@users.noreply.github.com>
2023-08-03 10:51:25 -0300

    Update Home.jsx

--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -3,6 +3,60 @@ import Buttons, { Boton } from "./Buttons";
 import './styles/boton.css'
 
 function Home() {
+  function probarFetch(){
+    
+// document.getElementById("registro").addEventListener("submit", function(event) {
+  // event.preventDefault(); // Evitar el envío del formulario por defecto
+
+  /*// Obtener los valores de los campos del formulario
+  var nombre = document.getElementById("name").value;
+  var usuario = document.getElementById("usuario").value;
+  var password = document.getElementById("password").value;
+  var confirmarPassword = document.getElementById("confirmarpassword").value;
+  var rol = document.getElementById("rol").value;
+
+  // Crear un objeto con los datos a enviar al backend
+  var data = {
+      nombre: nombre,
+      usuario: usuario,
+      password: password,
+      confirmarPassword: confirmarPassword,
+      rol: rol
+  };*/
+  var pruebaD = {
+    nombre: "juan",
+    usuario: "jmm",
+    password: 123,
+    confirmarPassword: 123,
+    rol: "usuario"
+};
+
+  // Enviar los datos al backend utilizando AJAX o Fetch
+  fetch('http://localhost:9000/api/CrearUsuario', {
+      method: "POST",
+      body: JSON.stringify(pruebaD),
+      headers: {
+          "Content-Type": "application/json"
+      }
+  })
+  .then(function(response) {
+      // Manejar la respuesta del backend
+      if (response.ok) {
+          // La solicitud se realizó correctamente
+          // Aquí puedes redirigir al usuario o realizar alguna otra acción
+          console.log(response)
+      } else {
+          // La solicitud falló, puedes manejar el error de alguna manera
+      }
+  })
+  .catch(function(error) {
+      // Manejar cualquier error de conexión u otro tipo de error
+  });
+// });
+
+
+  }
+
   return(
     <html lang="en">
       <head>
@@ -24,6 +78,7 @@ function Home() {
             <button><a href="/Register">Register</a></button>
             <button><a href="/Login">Log in</a></button>
         </div>
+        <button onClick={probarFetch}>Probar</button>
       </body>
     </html>
   );
