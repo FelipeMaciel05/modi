e216bbf1e0a2acdaa4104f5f1fe449c1b26dce97
FelipeMaciel05 <felimorita05@gmail.com>
2023-10-24 15:52:54 -0300

    grupos avance

--- src/Componentes/Crear.jsx
+++ src/Componentes/Crear.jsx
@@ -40,7 +40,7 @@ function CrearGrupo() {
     <html lang="en">
         <head>
         <meta charset="UTF-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />a
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Crear un grupo</title>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
--- src/Componentes/Grupo.jsx
+++ src/Componentes/Grupo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "./Header";
+import './styles/grupo-creado.css'
 
 
 function Grupo(){
@@ -19,14 +20,85 @@ function Grupo(){
         <body>
             <Header />
 
-            <div className="grupo">
-                <div className="caja-grupo">
-                    <form method="post">
-                        
-                    </form>
+            <div className="grupo-total">
+                <div className="caja-medicos-total">
+                    <div className="1">
+                    <div className="caja-medico-cabecera">
+                        <form method="post" className="form-de-grupo-creado">
+                            <div className="medico-cabecera">
+                                <div className="texto">
+                                    <p>Medico Cabecera</p>
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Diagnostico" />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Estudio Auxiliares" />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Indicaciones Medicas" />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Indicaciones Enfermeria" />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Evolucion" />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Devolucion al familiar " />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="submit" value="Enviar" />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="caja-enfermera">
+                        <form method="post" className="form-de-grupo-creado">
+                            <div className="enfermera">
+                                <div className="texto">
+                                    <p>Enfermera</p>
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Indicaciones Medicas" />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Evolucion" />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Devolucion al familiar" />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="submit" value="Enviar" />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    </div>
+                    
+                    <div className="caja-kinesiologo">
+                        <form method="post" className="form-de-grupo-creado">
+                            <div className="kinesiologo">
+                                <div className="texto">
+                                    <p>Kinesiologo</p>
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Indicaciones Medicas" />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Evolucion" />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="text" placeholder="Devolucion al familiar" />
+                                </div>
+                                <div className="caja-input">
+                                    <input type="submit" value="Enviar" />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-
         </body>
         </html>
     );
